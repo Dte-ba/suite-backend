8d218d773305f9b41f96a509f78b8f93a175d200
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-12-08 22:00:05 -0300

    Haciendo que los tests impriman la salida en colores.

--- suite/suite/settings.py
+++ suite/suite/settings.py
@@ -142,3 +142,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Para ejecutar test en colores.
+TEST_RUNNER="redgreenunittest.django.runner.RedGreenDiscoverRunner"
