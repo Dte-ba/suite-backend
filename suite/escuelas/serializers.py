f01553cc80918242174c2c3cd540ac7cf26b43b0
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-12-09 15:21:07 -0300

    Arreglo menor.

--- suite/escuelas/serializers.py
+++ suite/escuelas/serializers.py
@@ -5,8 +5,7 @@ import models
 class CustomSerializer(serializers.HyperlinkedModelSerializer):
 
     def get_field_names(self, declared_fields, info):
-        parent_class = super(CustomSerializer, self)
-        expanded_fields = perent_class.get_field_names(declared_fields, info)
+        expanded_fields = super(CustomSerializer, self).get_field_names(declared_fields, info)
 
         if getattr(self.Meta, 'extra_fields', None):
             return expanded_fields + self.Meta.extra_fields
