d2b243e3afb371a8feadc5200b0469e9fee77680
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-01-03 10:42:09 -0300

    Terminando de configurar staticFiles

--- suite/suite/settings.py
+++ suite/suite/settings.py
@@ -143,6 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/tmp/static/'
 
 # Para que el resultado de los tests pueda verse en colores.
 TEST_RUNNER="redgreenunittest.django.runner.RedGreenDiscoverRunner"
