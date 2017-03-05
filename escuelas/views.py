dad3b6c597f67940648553d60d3f19fed659e108
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-03-05 10:10:30 -0300

    Mejorando la pantalla principal del backend.

--- escuelas/views.py
+++ escuelas/views.py
@@ -1,12 +1,14 @@
 # coding: utf-8
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import viewsets
+
 import serializers
 import models
 
 def home(request):
-    return HttpResponse("Bienvenido a la API de SUITE, ingrese en <a href='/api'>/api</a> o <a href='/admin'>/admin</a> para más detalles.")
+    return render(request, 'home.html')
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
