de73ba97ffd65606ffaf09179dbe59bdaf5602f6
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-06-09 11:37:02 -0300

    Agregando los modelos nuevos al admin.

--- escuelas/admin.py
+++ escuelas/admin.py
@@ -6,6 +6,12 @@ admin.site.register(models.Escuela)
 admin.site.register(models.Evento)
 admin.site.register(models.Perfil)
 
+admin.site.register(models.TipoDeFinanciamiento)
+admin.site.register(models.Nivel)
+admin.site.register(models.TipoDeGestion)
+admin.site.register(models.Area)
+admin.site.register(models.Programa)
+
 class MunicipioInline(admin.TabularInline):
     model = models.Municipio
 
