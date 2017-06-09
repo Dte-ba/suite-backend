9cb08d152137267c518a24993fb235117e63d569
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-06-09 11:26:16 -0300

    Agregando areas y programas al importador de datos inicial.

--- escuelas/management/commands/cargar_datos.py
+++ escuelas/management/commands/cargar_datos.py
@@ -9,6 +9,8 @@ class Command(BaseCommand):
         self.crear_tipos_de_financiamiento()
         self.crear_niveles()
         self.crear_tipos_de_gestion()
+        self.crear_areas()
+        self.crear_programas()
 
     def crear_tipos_de_financiamiento(self):
         nombres = ["Nacional", "Provincial", "Municipal", "Propio"]
@@ -29,4 +31,24 @@ class Command(BaseCommand):
 
         for nombre in nombres:
             p, created = models.TipoDeGestion.objects.get_or_create(nombre=nombre)
+            print(p)
+
+    def crear_areas(self):
+        nombres = ["Urbana", "Rural"]
+
+        for nombre in nombres:
+            p, created = models.Area.objects.get_or_create(nombre=nombre)
+            print(p)
+
+    def crear_programas(self):
+        nombres = [
+            "Conectar Igualdad",
+            "PAD",
+            "Responsabilidad Empresarial",
+            "Primaria Digital",
+            "Escuelas del Futuro"
+            ]
+
+        for nombre in nombres:
+            p, created = models.Programa.objects.get_or_create(nombre=nombre)
             print(p)
