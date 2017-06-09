8328a3dd72b2b4b98ce3021f4266252ea56cbd4b
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-06-09 11:50:57 -0300

    Creando regiones desde el script de carga de datos inicial.

--- escuelas/management/commands/cargar_datos.py
+++ escuelas/management/commands/cargar_datos.py
@@ -2,15 +2,38 @@ from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
 from escuelas import models
 
+import requests
+
 class Command(BaseCommand):
     help = 'Genera todos los datos iniciales.'
 
     def handle(self, *args, **options):
+        self.importar_usuarios()
+
+        self.crear_regiones()
         self.crear_tipos_de_financiamiento()
         self.crear_niveles()
         self.crear_tipos_de_gestion()
         self.crear_areas()
         self.crear_programas()
+
+    def crear_regiones(self):
+        numeros = range(1, 26)
+
+        for n in numeros:
+            p, created = models.Region.objects.get_or_create(numero=n)
+            print(p)
+
+        p, created = models.Region.objects.get_or_create(numero=27)
+        print(p)
+
+    def importar_usuarios(self):
+        #print(self.obtener_datos_desde_api('usuarios/'))
+        pass
+
+    def obtener_datos_desde_api(self, data):
+        url = 'http://suite.dtelab.com.ar/suite/datasuite/api/' + data
+        return requests.get(url).json()
 
     def crear_tipos_de_financiamiento(self):
         nombres = ["Nacional", "Provincial", "Municipal", "Propio"]
