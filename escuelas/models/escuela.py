2f2e59de44413bf1652d1106863762511e6a42d8
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-06-09 11:00:40 -0300

    Eliminando telefono y directorio del modelo de Escuela.

--- escuelas/models/escuela.py
+++ escuelas/models/escuela.py
@@ -4,8 +4,6 @@ from django.db import models
 class Escuela(models.Model):
     cue = models.CharField(max_length=8, db_index=True)
     nombre = models.CharField(max_length=100)
-    telefono = models.CharField(max_length=100)
-    director = models.CharField(max_length=50, default="")
 
     def __unicode__(self):
         return self.nombre
