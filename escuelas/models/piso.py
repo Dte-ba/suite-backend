df453cfd9e2631fb3040d4bc373245379f465127
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-07-03 16:50:12 -0300

    Corrigiendo modelo de piso.

--- escuelas/models/piso.py
+++ escuelas/models/piso.py
@@ -4,9 +4,9 @@ from django.db import models
 class Piso(models.Model):
     servidor = models.CharField(max_length=255)
     serie = models.CharField(max_length=255, default=None, blank=True, null=True)
-    ups = models.BooleanField()
-    rack = models.BooleanField()
-    estado = models.BooleanField()
+    ups = models.BooleanField(default=False)
+    rack = models.BooleanField(default=False)
+    estado = models.BooleanField(default=False)
 
     def __unicode__(self):
         return u"Piso: " + self.servidor + " Serie " + self.serie
