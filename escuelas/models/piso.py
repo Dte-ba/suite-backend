3fd3706e76b6577a1e6f2f143b9461a139f5ebae
Iván Hoffmann <digital.h@gmail.com>
2017-07-25 10:59:52 -0300

    Implementando FileField a piso para poder subir la llave del servidor

--- escuelas/models/piso.py
+++ escuelas/models/piso.py
@@ -1,12 +1,16 @@
 # coding: utf-8
 from django.db import models
 
+def upload_to(filename):
+    return 'piso_llave_servidor/{0}'.format(filename)
+
 class Piso(models.Model):
     servidor = models.CharField(max_length=255)
     serie = models.CharField(max_length=255, default=None, blank=True, null=True)
     ups = models.BooleanField(default=False)
     rack = models.BooleanField(default=False)
     estado = models.BooleanField(default=False)
+    llave = models.FileField(upload_to='piso_llave_servidor/', blank=True, null=True)
 
     class Meta:
         db_table = 'pisos'
