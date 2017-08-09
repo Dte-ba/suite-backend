ac48c09c195a500213e570f52ff3f3dcf25d785b
Iván Hoffmann <digital.h@gmail.com>
2017-08-09 18:18:33 -0300

    Agregando campos Objetivo, Cantidad de Participantes y Traslado a Evento

--- escuelas/models/evento.py
+++ escuelas/models/evento.py
@@ -11,9 +11,14 @@ class Evento(models.Model):
     fin = models.TimeField(default='00:00:00')
     todoElDia = models.BooleanField(default=False)
 
+    objetivo = models.TextField(max_length=4096, default=None, blank=True, null=True)
+
     responsable = models.ForeignKey('Perfil', on_delete=models.CASCADE, related_name='eventos', default=None, blank=True, null=True)
     escuela = models.ForeignKey('Escuela', on_delete=models.CASCADE, related_name='eventos', default=None, blank=True, null=True)
     acompaniantes = models.ManyToManyField('Perfil', related_name='eventos_acompaniantes', default=None, blank=True)
+
+    cantidadDeParticipantes = models.CharField(max_length=256,default=None, blank=True, null=True)
+    requiereTraslado = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.titulo
