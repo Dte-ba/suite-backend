e13a6bfc8592b3ebe304541e99d2b301682e4e7b
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-11-16 09:51:09 -0300

    Poniendo el límite de elementos por página de las listas y tablas a 15 por omisión (en lugar de 10).

--- suite/settings.py
+++ suite/settings.py
@@ -93,7 +93,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 15,
     'EXCEPTION_HANDLER': 'rest_framework_json_api.exceptions.exception_handler',
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework_json_api.pagination.PageNumberPagination',
