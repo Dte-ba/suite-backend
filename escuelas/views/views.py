0eaf7eb7a198c8c09e04f665f55b41363120be79
Iván Hoffmann <digital.h@gmail.com>
2018-04-19 10:58:11 -0300

    Agregando query por región a estadísticas de escuela

--- escuelas/views/views.py
+++ escuelas/views/views.py
@@ -207,6 +207,11 @@ class EscuelaViewSet(viewsets.ModelViewSet):
     @list_route(methods=['get'])
     def estadistica(self, request):
         queryset = models.Escuela.objects.filter(padre__isnull=True)
+        filtro_region = self.request.query_params.get('localidad__distrito__region__numero')
+
+        if filtro_region:
+            filtro = Q(localidad__distrito__region__numero=filtro_region) | Q(cue=60000000)
+            queryset = queryset.filter(filtro)
 
         estadisticas = {
             "total": queryset.count(),
