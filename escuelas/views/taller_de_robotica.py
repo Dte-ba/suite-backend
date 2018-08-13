e6726fa461afe1d6729413234d4010a0cec2a05d
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-08-13 14:18:25 -0300

    Implementando filtro para obtener talleres por area.

--- escuelas/views/taller_de_robotica.py
+++ escuelas/views/taller_de_robotica.py
@@ -3,8 +3,10 @@ from __future__ import unicode_literals
 from rest_framework import viewsets
 
 from escuelas import models, serializers
-
+from django_filters.rest_framework import DjangoFilterBackend
 
 class TallerDeRoboticaViewSet(viewsets.ModelViewSet):
     queryset = models.TallerDeRobotica.objects.all()
     serializer_class = serializers.TallerDeRoboticaSerializer
+    filter_backends = [DjangoFilterBackend]
+    filter_fields = ['area__id']
