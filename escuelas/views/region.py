28f653157d27a52ec9b966c972d3f65d3f6c68b3
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-08-21 15:13:47 -0300

    Corrigiendo filtro de la región 27.

--- escuelas/views/region.py
+++ escuelas/views/region.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from __future__ import unicode_literals
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 
 from escuelas import models, serializers
 
@@ -9,3 +10,6 @@ class RegionViewSet(viewsets.ModelViewSet):
     resource_name = 'regiones'
     queryset = models.Region.objects.all()
     serializer_class = serializers.RegionSerializer
+
+    filter_backends = [DjangoFilterBackend]
+    filter_fields = ['numero']
