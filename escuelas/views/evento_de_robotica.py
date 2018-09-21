1508495ca1f8740e0ecef3b960000b427d128f39
Iván Hoffmann <digital.h@gmail.com>
2018-09-21 11:53:46 -0300

    Agregando criterio de exportación al exportador de talleres a xls.

--- escuelas/views/evento_de_robotica.py
+++ escuelas/views/evento_de_robotica.py
@@ -380,8 +380,13 @@ class EventoDeRoboticaViewSet(viewsets.ModelViewSet):
         inicio = self.request.query_params.get('inicio', None)
         fin = self.request.query_params.get('fin', None)
         region = self.request.query_params.get('region', None)
+        criterio = self.request.query_params.get('criterio', None)
 
-        eventos = models.EventoDeRobotica.objects.filter(fecha__range=(inicio, fin))
+        if criterio:
+            if criterio == "fechaDeRealizacion":
+                eventos = models.EventoDeRobotica.objects.filter(fecha__range=(inicio, fin))
+            else:
+                eventos = models.EventoDeRobotica.objects.filter(fecha_de_creacion__range=(inicio, fin))
 
         if region:
             eventos = eventos.filter(escuela__localidad__distrito__region__numero=region)
@@ -396,34 +401,35 @@ class EventoDeRoboticaViewSet(viewsets.ModelViewSet):
         font_style = xlwt.XFStyle()
         font_style.font.bold = True
 
-        columns = ['Fecha', 'Hora Inicio', 'Hora Fin', 'Título', 'Área', 'Curso', 'Sección', 'Cant. de Alumnos', 'Tallerista', 'Escuela', 'CUE', 'Región', 'Distrito', 'Localidad', 'Docente a Cargo', 'Se realizó el taller?', 'Motivo si NO se realizó', 'Acta', 'Estado', 'Observaciones', 'Fecha de Creación']
+        columns = ['Fecha de Creación', 'Fecha', 'Hora Inicio', 'Hora Fin', 'Título', 'Área', 'Curso', 'Sección', 'Cant. de Alumnos', 'Tallerista', 'Escuela', 'CUE', 'Región', 'Distrito', 'Localidad', 'Docente a Cargo', 'Se realizó el taller?', 'Motivo si NO se realizó', 'Acta', 'Estado', 'Observaciones']
         col_num = 2 # 0 y 1 son obligatorias
 
         # Escribir los headers
         for col_num in range(len(columns)):
             ws.write(0, col_num, columns[col_num], font_style)
 
-        ws.col(0).width = 256 * 12 # Fecha
-        ws.col(1).width = 256 * 12 # Hora Inicio
-        ws.col(2).width = 256 * 12 # Hora Fin
-        ws.col(3).width = 600 * 12 # Titulo
-        ws.col(4).width = 400 * 12 # Área
-        ws.col(5).width = 256 * 12 # Curso
-        ws.col(6).width = 256 * 12 # Sección
-        ws.col(7).width = 256 * 12 # Cantidad de Alumnos
-        ws.col(8).width = 600 * 12 # Tallerista
-        ws.col(9).width = 600 * 12 # Escuela
-        ws.col(10).width = 256 * 12 # CUE
-        ws.col(11).width = 200 * 12 # Región
-        ws.col(12).width = 400 * 12 # Distrito
-        ws.col(13).width = 400 * 12 # Localidad
-        ws.col(14).width = 600 * 12 # Docente a Cargo
-        ws.col(15).width = 600 * 12 # Se pudo realizar el taller?
-        ws.col(16).width = 600 * 12 # Si no se pudo, motivo.
-        ws.col(17).width = 256 * 12 # Acta
-        ws.col(18).width = 300 * 12 # Estado
-        ws.col(19).width = 600 * 12 # Observaciones
-        ws.col(20).width = 256 * 12 # Fecha de Creación
+
+        ws.col(0).width = 512 * 12 # Fecha de Creación
+        ws.col(1).width = 256 * 12 # Fecha
+        ws.col(2).width = 256 * 12 # Hora Inicio
+        ws.col(3).width = 256 * 12 # Hora Fin
+        ws.col(4).width = 600 * 12 # Titulo
+        ws.col(5).width = 400 * 12 # Área
+        ws.col(6).width = 256 * 12 # Curso
+        ws.col(7).width = 256 * 12 # Sección
+        ws.col(8).width = 256 * 12 # Cantidad de Alumnos
+        ws.col(9).width = 600 * 12 # Tallerista
+        ws.col(10).width = 600 * 12 # Escuela
+        ws.col(11).width = 256 * 12 # CUE
+        ws.col(12).width = 200 * 12 # Región
+        ws.col(13).width = 400 * 12 # Distrito
+        ws.col(14).width = 400 * 12 # Localidad
+        ws.col(15).width = 600 * 12 # Docente a Cargo
+        ws.col(16).width = 600 * 12 # Se pudo realizar el taller?
+        ws.col(17).width = 600 * 12 # Si no se pudo, motivo.
+        ws.col(18).width = 256 * 12 # Acta
+        ws.col(19).width = 300 * 12 # Estado
+        ws.col(20).width = 600 * 12 # Observaciones
 
         font_style = xlwt.XFStyle()
 
@@ -467,27 +473,27 @@ class EventoDeRoboticaViewSet(viewsets.ModelViewSet):
                 estado = "Abierto"
 
             row_num += 1
-            ws.write(row_num, 0, fecha, font_style)
-            ws.write(row_num, 1, hora_inicio, font_style)
-            ws.write(row_num, 2, hora_fin, font_style)
-            ws.write(row_num, 3, titulo, font_style)
-            ws.write(row_num, 4, area, font_style)
-            ws.write(row_num, 5, curso, font_style)
-            ws.write(row_num, 6, seccion, font_style)
-            ws.write(row_num, 7, cantidad_de_alumnos, font_style)
-            ws.write(row_num, 8, tallerista, font_style)
-            ws.write(row_num, 9, escuela, font_style)
-            ws.write(row_num, 10, cue, font_style)
-            ws.write(row_num, 11, region, font_style)
-            ws.write(row_num, 12, distrito, font_style)
-            ws.write(row_num, 13, localidad, font_style)
-            ws.write(row_num, 14, docente_a_cargo, font_style)
-            ws.write(row_num, 15, se_dio_el_taller, font_style)
-            ws.write(row_num, 16, motivo, font_style)
-            ws.write(row_num, 17, acta, font_style)
-            ws.write(row_num, 18, estado, font_style)
-            ws.write(row_num, 19, observaciones, font_style)
-            ws.write(row_num, 20, fecha_de_creacion, font_style)
+            ws.write(row_num, 0, fecha_de_creacion, font_style)
+            ws.write(row_num, 1, fecha, font_style)
+            ws.write(row_num, 2, hora_inicio, font_style)
+            ws.write(row_num, 3, hora_fin, font_style)
+            ws.write(row_num, 4, titulo, font_style)
+            ws.write(row_num, 5, area, font_style)
+            ws.write(row_num, 6, curso, font_style)
+            ws.write(row_num, 7, seccion, font_style)
+            ws.write(row_num, 8, cantidad_de_alumnos, font_style)
+            ws.write(row_num, 9, tallerista, font_style)
+            ws.write(row_num, 10, escuela, font_style)
+            ws.write(row_num, 11, cue, font_style)
+            ws.write(row_num, 12, region, font_style)
+            ws.write(row_num, 13, distrito, font_style)
+            ws.write(row_num, 14, localidad, font_style)
+            ws.write(row_num, 15, docente_a_cargo, font_style)
+            ws.write(row_num, 16, se_dio_el_taller, font_style)
+            ws.write(row_num, 17, motivo, font_style)
+            ws.write(row_num, 18, acta, font_style)
+            ws.write(row_num, 19, estado, font_style)
+            ws.write(row_num, 20, observaciones, font_style)
 
         wb.save(response)
         return(response)
